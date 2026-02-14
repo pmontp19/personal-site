63c41dfefbffe873cdac175de30b1e7526fe55fd
Pere <pmontpeo@goil.app>
2026-02-14 23:57:59 +0100

    feat: add new fonts and images; refactor blog and contact sections

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,11 +2,13 @@ import { defineCollection, z } from "astro:content";
 
 const blog = defineCollection({
     type: "content",
-    schema: z.object({
+    schema: ({ image }) => z.object({
         title: z.string(),
         description: z.string(),
         date: z.coerce.date(),
-        draft: z.boolean().optional()
+        draft: z.boolean().optional(),
+        image: image().optional(),
+        imageAlt: z.string().optional(),
     }),
 });
 
