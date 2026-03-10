abeba0586426e6695f407b1d2819b459d797a4be
Pere <pmontpeo@goil.app>
2026-03-10 16:46:37 +0100

    feat: add rehype-slug and remark-heading-id plugins to markdown configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,15 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import mdx from '@astrojs/mdx';
+import rehypeSlug from 'rehype-slug';
+import remarkHeadingId from 'remark-heading-id';
 import rehypeExternalLinks from './src/plugins/rehype-external-links.ts';
 
 export default defineConfig({
   site: 'https://peremontpeo.dev',
   integrations: [sitemap(), mdx()],
   markdown: {
-    rehypePlugins: [rehypeExternalLinks],
+    remarkPlugins: [remarkHeadingId],
+    rehypePlugins: [rehypeSlug, rehypeExternalLinks],
   },
 });
